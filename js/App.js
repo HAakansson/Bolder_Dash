9474544c67f8e0402200db5f5e884a19545d7228
AAntonSB <anton.s.bergqvist@gmail.com>
2019-11-27 13:08:43 +0100

    Movement

--- js/App.js
+++ js/App.js
@@ -13,3 +13,26 @@ export default {
         </div>
         ` 
 }
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+    switch (e.code) {
+        case "ArrowUp":
+            grid.goUp();
+            break;
+        case "ArrowDown":
+            grid.goDown();
+            break;
+        case "ArrowLeft":
+            grid.goLeft();
+            break;
+        case "ArrowRight":
+            grid.goRight();
+            break;
+
+        default:
+	    //Do nothing.
+            break;
+    }
+}
