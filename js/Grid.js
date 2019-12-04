1187fc3685a8ae2b9b36bba479d408a68768b6b0
AAntonSB <anton.s.bergqvist@gmail.com>
2019-12-04 10:41:13 +0100

    Player movement
    sick

--- js/Grid.js
+++ js/Grid.js
@@ -14,7 +14,6 @@ export default {
         v-for="(tile, i) in flatTiles"
         v-bind:position="tile"
         v-bind:key="'tile' + i + tile.x + tile.y + tile.background"
-        v-on:keyup.enter='movePlayer'
         ></tile>
     </div>
     `,
@@ -27,11 +26,14 @@ export default {
         }
     },
 
+    properties() {
+        KeyboardEvent
+    },
+
     computed: {
         flatTiles() {
             return this.tiles.flat()
         },
-
         //    image() {
         //         return this.tileState[].tileImage
         //    }
@@ -47,11 +49,14 @@ export default {
                     y: row,
                     background: Tile.dirt
                 }
+                if (position.x === 1 && position.y === 1) {
+                    position.background = Tile.player
+                }
                 if (
                     position.y === 0 || position.y === this.gridSize - 1 ||
                     position.x === 0 || position.x === this.gridSize - 1) {
                     position.background = Tile.brick
-                } else if (Math.random() > 0.8) {
+                } else if (Math.random() > 0.8 && position.x !== 1 && position.y !== 1) {
                     position.background = Tile.boulder
                 }
 
@@ -73,14 +78,11 @@ export default {
 
     updated() {
         console.log("The grid has been changed");
-
         this.updateRollingStones();
-        // this.updatePlayerMovement()
+        //this.updatePlayerMovement()
         // If the player moves, we should call forceRender
     },
     methods: {
-        movePlayer: function() {
-        },
         //Inbyggd vue metod
         start: function () {
             this.forceRender();
@@ -91,7 +93,80 @@ export default {
             this.renderTimeout = setTimeout(() => {
                 // This will make the component re-render
                 Vue.set(this.tiles, 0, this.tiles[0]);
-            }, 1000)
+            }, 20)
+        },
+
+        updatePlayerMovement: function (direction) {
+            if (direction === 'right' || direction === 'up') {
+                for (let col = this.tiles.length - 1; col >= 0; col--) {
+                    for (let row = 0; row < this.tiles.length; row++) {
+                        const tile = this.tiles[row][col];
+                        if (direction === 'right') {
+                            if (tile.background == Tile.player) {
+                                const moveRight = this.tiles[row][col + 1];
+                                const checkIfEmpty = this.tiles[row][col + 2]
+                                if (moveRight.background !== Tile.brick &&
+                                    moveRight.background !== Tile.boulder) {
+                                    tile.background = Tile.empty;
+                                    moveRight.background = Tile.player;
+                                    this.forceRender();
+                                } else if (moveRight === Tile.boulder && checkIfEmpty.background === Tile.empty) {
+                                    moveRight.background = Tile.player;
+                                    checkIfEmpty.background = Tile.boulder;
+                                    tile.background = Tile.empty;
+                                    this.forceRender();
+                                }
+                            }
+                        } else if (direction === 'up') {
+                            if (tile.background === Tile.player) {
+                                const moveUp = this.tiles[row-1][col]
+                                if (moveUp.background !== Tile.brick &&
+                                    moveUp.background !== Tile.boulder) {
+                                    tile.background = Tile.empty;
+                                    moveUp.background = Tile.player;
+                                    this.forceRender();
+                                }
+                            }
+                        }
+                    }
+                }
+            } else if (direction === 'left' || direction === 'down') {
+                for (let row = this.tiles.length - 1; row >= 0; row--) {
+                    for (let col = 0; col < this.tiles.length; col++) {
+                        const tile = this.tiles[row][col];
+                        switch (direction) {
+                            case 'left':
+                                console.log('goin left')
+                                if (tile.background === Tile.player) {
+                                    const moveLeft = this.tiles[row][col - 1];
+                                    const checkIfEmpty = this.tiles[row][col - 2];
+                                    if (moveLeft.background !== Tile.brick &&
+                                        moveLeft.background !== Tile.boulder) {
+                                        tile.background = Tile.empty;
+                                        moveLeft.background = Tile.player;
+                                        this.forceRender();
+                                    } else if (moveRight === Tile.boulder && checkIfEmpty.background === Tile.empty) {
+                                        moveLeft.background = Tile.player;
+                                        checkIfEmpty.background = Tile.boulder;
+                                        tile.background = Tile.empty;
+                                        this.forceRender();
+                                    }
+                                } break
+                            case 'down':
+                                if (tile.background === Tile.player) {
+                                    const moveDown = this.tiles[row + 1][col];
+                                    if (moveDown.background !== Tile.brick &&
+                                        moveDown.background !== Tile.boulder) {
+                                        tile.background = Tile.empty;
+                                        moveDown.background = Tile.player;
+                                        this.forceRender();
+                                    }
+                                }
+                                break
+                        }
+                    }
+                }
+            }
         },
 
         updateRollingStones: function () {
@@ -146,6 +221,5 @@ export default {
                 }
             }
         }
-
     }
 }
--- js/main.js
+++ js/main.js
@@ -3,5 +3,4 @@ import App from './App.js'
 const v = new Vue({
     render: h => h(App)
 }).$mount('#app')
-
 Vue.config.performance = true;
